34a8fd6753d581b4efeb02ea2894ef5fdc76fe57
shlomi <shl.lev.ks@gmail.com>
2021-11-08 15:56:00 +0200

    init- spring batch to parse csv to db

---
+++ src/main/java/com/ks/ipldashboard/dto/MatchDTO.java
@@ -0,0 +1,4 @@
+package com.ks.ipldashboard.dto;
+
+public class MatchDTO {
+}
---
+++ src/test/java/com/ks/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
