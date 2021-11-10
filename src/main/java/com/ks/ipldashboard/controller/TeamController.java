82beb9d870479dc270aa0f67328bbc691a4fd2d2
shlomi <shl.lev.ks@gmail.com>
2021-11-10 18:08:17 +0200

    start react-view, fix cross-origin.

--- src/main/java/com/ks/ipldashboard/controller/TeamController.java
+++ src/main/java/com/ks/ipldashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.ks.ipldashboard.model.Team;
 import com.ks.ipldashboard.repository.MatchRepository;
 import com.ks.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
