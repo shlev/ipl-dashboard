352e2f641157d0de5d512e6589276aead11896e3
shlomi <shl.lev.ks@gmail.com>
2021-11-13 18:02:04 +0200

    add ui for team page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import { TeamPage } from "./pages/TeamPage";
-
+import { MatchPage } from "./pages/MatchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Test } from "./components/Test";
 function App() {
   return (
     <div className="App">
-      <TeamPage></TeamPage>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Test />} />
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
